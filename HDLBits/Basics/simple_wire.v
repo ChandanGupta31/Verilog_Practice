21853765c5036affa33e6b4fc27e9c08e6bc4690
Chandan <chandankumarguptabgt@gmail.com>
2026-08-15 07:30:42 +0530

    Solved Simple Wire Problem

---
+++ HDLBits/Basics/simple_wire.v
@@ -0,0 +1,11 @@
+// ==========================================
+// Problem: Simple Wire
+// Link: https://hdlbits.01xz.net/wiki/Wire
+// Concept: Simple wire assignment
+// ==========================================
+
+module top_module( input in, output out );
+
+	assign out=in;
+    
+endmodule
