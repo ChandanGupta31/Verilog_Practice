8eb33daa78d5d714464d05a688c482f1a95788b3
Chandan <chandankumarguptabgt@gmail.com>
2026-09-05 06:10:13 +0530

    Multiplexer questions

---
+++ HDLBits/Circuits/Combinational Logics/mux2to1v.v
@@ -0,0 +1,11 @@
+// ==========================================
+// Problem: Mux2to1v
+// Link: https://hdlbits.01xz.net/wiki/Mux2to1v
+// ==========================================
+
+module top_module( 
+    input [99:0] a, b,
+    input sel,
+    output [99:0] out );
+	assign out = sel ? b : a;
+endmodule
